85a48ff975ea51a81cb4dd2cf95b87a83c2a0fb7
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-03 14:12:29 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'ListBox1.Items.Add("Redmi")
+        'ListBox1.Items.Add("Popi")
+
+        Dim myitem
+
+        myitem = InputBox("Enter your favorite phone")
+        ListBox1.Items.Add(myitem)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim myitem2
+
+        myitem2 = InputBox("Enter name of the phone to remove")
+        ListBox1.Items.Remove(myitem2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
